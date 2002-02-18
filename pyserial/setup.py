e319968296f4ff442c214779f3dae502335472ba
cliechti <cliechti@f19166aa-fa4f-0410-85c2-fa1106f25c8a>
2002-02-18 19:33:03 +0000

    updated for 1.12

--- pyserial/setup.py
+++ pyserial/setup.py
@@ -5,7 +5,7 @@ from glob import glob
 setup(
     name="pyserial",
     description="Python Serial Port Extension",
-    version="1.11",
+    version="1.12",
     author="Chris Liechti",
     author_email="cliechti@gmx.net",
     url="http://pyserial.sourceforge.net/",
