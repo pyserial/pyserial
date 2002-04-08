a90558afba825af453eee409f82ae5ea5cb8cf3c
cliechti <cliechti@f19166aa-fa4f-0410-85c2-fa1106f25c8a>
2002-04-08 23:31:37 +0000

    updated for 1.13 added long description and licese info

--- pyserial/setup.py
+++ pyserial/setup.py
@@ -5,9 +5,11 @@ from glob import glob
 setup(
     name="pyserial",
     description="Python Serial Port Extension",
-    version="1.12",
+    version="1.13",
     author="Chris Liechti",
     author_email="cliechti@gmx.net",
     url="http://pyserial.sourceforge.net/",
     packages=['serial'],
+    license="Python",
+    long_description="Python Serial Port Extension for Win32, Linux, BSD, Jython"
 )
