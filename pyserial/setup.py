8db88b6d7e691d5e616915cfeeb117825de0a37f
cliechti <cliechti@f19166aa-fa4f-0410-85c2-fa1106f25c8a>
2003-09-10 20:14:53 +0000

    updated clasifiers for PyPi

--- pyserial/setup.py
+++ pyserial/setup.py
@@ -13,5 +13,18 @@ setup(
     url="http://pyserial.sourceforge.net/",
     packages=['serial'],
     license="Python",
-    long_description="Python Serial Port Extension for Win32, Linux, BSD, Jython"
+    long_description="Python Serial Port Extension for Win32, Linux, BSD, Jython",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Topic :: Communications',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Terminals :: Serial',
+    ],
 )
