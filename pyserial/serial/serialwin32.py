8099c7aa70fc1acbf22508e62d64fe6617e138e1
cliechti <cliechti@f19166aa-fa4f-0410-85c2-fa1106f25c8a>
2007-12-11 19:08:38 +0000

    bugfix (found by R. Mitchell)

--- pyserial/serial/serialwin32.py
+++ pyserial/serial/serialwin32.py
@@ -11,7 +11,7 @@ import win32event # We use events and the WaitFor[Single|Multiple]Objects functi
 import win32con   # constants.
 from serialutil import *
 
-VERSION = "$Revision: 1.34 $".split()[1]     #extract CVS version
+VERSION = "$Revision: 1.35 $".split()[1]     #extract CVS version
 
 #from winbase.h. these should realy be in win32con
 MS_CTS_ON  = 16
@@ -60,7 +60,7 @@ class Serial(SerialBase):
         self._orgTimeouts = win32file.GetCommTimeouts(self.hComPort)
 
         self._rtsState = win32file.RTS_CONTROL_ENABLE
-        self._dtrState = win32file.RTS_CONTROL_ENABLE
+        self._dtrState = win32file.DTR_CONTROL_ENABLE
 
         self._reconfigurePort()
         
