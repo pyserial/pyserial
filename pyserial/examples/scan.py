0400feb67e14f86088dcf6d5a6f75674880f6de5
cliechti <cliechti@f19166aa-fa4f-0410-85c2-fa1106f25c8a>
2009-07-21 22:41:46 +0000

    add scan example for linux

--- pyserial/examples/scan.py
+++ pyserial/examples/scan.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python
-"""Scan for serial ports.
-Part of pySerial (http://pyserial.sf.net)  (C)2002-2003 <cliechti@gmx.net>
+#! /usr/bin/env python
+"""\
+Scan for serial ports.
+
+Part of pySerial (http://pyserial.sf.net)
+(C) 2002-2003 <cliechti@gmx.net>
 
 The scan function of this module tries to open each port number
 from 0 to 255 and it builds a list of those ports where this was
@@ -16,7 +19,7 @@ def scan():
         try:
             s = serial.Serial(i)
             available.append( (i, s.portstr))
-            s.close()   #explicit close 'cause of delayed GC in java
+            s.close()   # explicit close 'cause of delayed GC in java
         except serial.SerialException:
             pass
     return available
---
+++ pyserial/examples/scanlinux.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+"""\
+Scan for serial ports. Linux specific variant that also includes USB/Serial
+adapters.
+
+Part of pySerial (http://pyserial.sf.net)
+(C) 2009 <cliechti@gmx.net>
+"""
+
+import serial
+import glob
+
+def scan():
+    """scan for available ports. return a list of device names."""
+    return glob.glob('/dev/ttyS*') + glob.glob('/dev/ttyUSB*')
+
+if __name__=='__main__':
+    print "Found ports:"
+    for name in scan():
+        print name
