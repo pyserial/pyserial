5735a079f93830b1c3e608a6703d6a006065a06e
cliechti <cliechti@f19166aa-fa4f-0410-85c2-fa1106f25c8a>
2009-10-26 23:57:48 +0000

    bugfix 2886763

--- pyserial/serial/serialwin32.py
+++ pyserial/serial/serialwin32.py
@@ -25,12 +25,15 @@ class Win32Serial(SerialBase):
     BAUDRATES = (50, 75, 110, 134, 150, 200, 300, 600, 1200, 1800, 2400, 4800,
                  9600, 19200, 38400, 57600, 115200)
 
+    def __init__(self, *args, **kwargs):
+        self.hComPort = None
+        SerialBase.__init__(self, *args, **kwargs)
+
     def open(self):
         """Open port with current settings. This may throw a SerialException
            if the port cannot be opened."""
         if self._port is None:
             raise SerialException("Port must be configured before it can be used.")
-        self.hComPort = None
         # the "\\.\COMx" format is required for devices other than COM1-COM8
         # not all versions of windows seem to support this properly
         # so that the first few ports are used with the DOS device name
