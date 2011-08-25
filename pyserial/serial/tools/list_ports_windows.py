d0c9e5808992ef9a38be2009de035a83d2fcccd0
cliechti <cliechti@f19166aa-fa4f-0410-85c2-fa1106f25c8a>
2011-08-25 08:22:03 +0000

    fix syntax error

--- pyserial/serial/tools/list_ports_windows.py
+++ pyserial/serial/tools/list_ports_windows.py
@@ -50,7 +50,6 @@ class GUID(ctypes.Structure):
         ('Data2', WORD),
         ('Data3', WORD),
         ('Data4', BYTE*8),
-),
     ]
     def __str__(self):
         return "{%08x-%04x-%04x-%s-%s}" % (
