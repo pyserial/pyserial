47c9d1693a5c7f89c5aecaff9b03a09638bde5dc
cliechti <cliechti@f19166aa-fa4f-0410-85c2-fa1106f25c8a>
2012-03-29 21:58:54 +0000

    ignore case in description and hwid too

--- pyserial/serial/tools/list_ports.py
+++ pyserial/serial/tools/list_ports.py
@@ -39,7 +39,7 @@ def grep(regexp):
     same tuples as comport() would do.
     """
     for port, desc, hwid in comports():
-        if re.search(regexp, port, re.I) or re.search(regexp, desc) or re.search(regexp, hwid):
+        if re.search(regexp, port, re.I) or re.search(regexp, desc, re.I) or re.search(regexp, hwid, re.I):
             yield port, desc, hwid
 
 
