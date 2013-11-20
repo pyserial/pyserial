6b7f7bdc8c0691fc09bab8b29a583892e173468b
cliechti <cliechti@f19166aa-fa4f-0410-85c2-fa1106f25c8a>
2013-11-20 21:37:02 +0000

    include url handlers

--- pyserial/examples/setup-rfc2217_server-py2exe.py
+++ pyserial/examples/setup-rfc2217_server-py2exe.py
@@ -11,12 +11,15 @@ sys.argv.extend("py2exe --bundle 1".split())
 setup(
     name='rfc2217_server',
     zipfile=None,
-    options = {"py2exe":
-        {
-            'dist_dir': 'bin',
-            'excludes': ['javax.comm'],
-            'compressed': 1,
-        }
+    options = {"py2exe": {
+        'dll_excludes': [],
+        'includes': [
+                'serial.urlhandler.protocol_hwgrep', 'serial.urlhandler.protocol_rfc2217',
+                'serial.urlhandler.protocol_socket', 'serial.urlhandler.protocol_loop',],
+        'dist_dir': 'bin',
+        'excludes': ['serialjava', 'serialposix', 'serialcli'],
+        'compressed': 1,
+        },
     },
     console = [
         "rfc2217_server.py",
