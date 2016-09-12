122a93209cd7f0ef3d6089f1c5d70a74ca25de80
Chris Liechti <cliechti@gmx.net>
2016-09-12 23:42:51 +0200

    test: add tests for to_bytes and iterbytes

---
+++ test/test_util.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#
+# This file is part of pySerial - Cross platform serial port support for Python
+# (C) 2016 Chris Liechti <cliechti@gmx.net>
+#
+# SPDX-License-Identifier:    BSD-3-Clause
+"""\
+Tests for utility functions of serualutil.
+"""
+
+import os
+import unittest
+import serial
+import time
+
+
+class Test_util(unittest.TestCase):
+    """Test serial utility functions"""
+
+    def test_to_bytes(self):
+        self.assertEqual(serial.to_bytes([1, 2, 3]), b'\x01\x02\x03')
+        self.assertEqual(serial.to_bytes(b'\x01\x02\x03'), b'\x01\x02\x03')
+        self.assertEqual(serial.to_bytes(bytearray([1,2,3])), b'\x01\x02\x03')
+        self.assertRaises(TypeError, serial.to_bytes, u'hello')
+
+    def test_iterbytes(self):
+        self.assertEqual(list(serial.iterbytes(b'\x01\x02\x03')), [b'\x01', b'\x02', b'\x03'])
+
+
+
+if __name__ == '__main__':
+    import sys
+    sys.stdout.write(__doc__)
+    if len(sys.argv) > 1:
+        PORT = sys.argv[1]
+    sys.argv[1:] = ['-v']
+    # When this module is executed from the command-line, it runs all its tests
+    unittest.main()
