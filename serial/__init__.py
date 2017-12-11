44ab633a56ff0adab9765bfb6296ea4f87636d31
jamessynge <james.synge@gmail.com>
2017-12-10 21:36:31 -0500

    Add support to serial_for_url for handlers with fully qualified package names.

--- serial/__init__.py
+++ serial/__init__.py
@@ -38,6 +38,28 @@ protocol_handler_packages = [
 ]
 
 
+def _find_module_for_protocol(protocol, packages):
+    """Return the module that contains the handler for the specified protocol.
+    
+    If the protocol appears to be a fully qualified Python module (i.e. a.b.c), then use
+    the package name (e.g. 'a.b' from the example 'a.b.c'), and use the final module name
+    as the protocol name (e.g. 'c').
+    """
+    parts = protocol.split('.')
+    if len(parts) > 1 and '' not in parts:
+        # Treat the protocol as a full Python package name/path followed by the protocol name.
+        protocol = parts[-1]
+        packages = ['.'.join(parts[0:-1])]
+    module_name = '.protocol_{}'.format(protocol)
+    for package_name in packages:
+        try:
+            importlib.import_module(package_name)
+            return importlib.import_module(module_name, package_name)
+        except ImportError:
+            continue
+    return None
+
+
 def serial_for_url(url, *args, **kwargs):
     """\
     Get an instance of the Serial class, depending on port/url. The port is not
@@ -66,19 +88,12 @@ def serial_for_url(url, *args, **kwargs):
         # if it is an URL, try to import the handler module from the list of possible packages
         if '://' in url_lowercase:
             protocol = url_lowercase.split('://', 1)[0]
-            module_name = '.protocol_{}'.format(protocol)
-            for package_name in protocol_handler_packages:
-                try:
-                    importlib.import_module(package_name)
-                    handler_module = importlib.import_module(module_name, package_name)
-                except ImportError:
-                    continue
+            handler_module = _find_module_for_protocol(protocol, protocol_handler_packages)
+            if handler_module:
+                if hasattr(handler_module, 'serial_class_for_url'):
+                    url, klass = handler_module.serial_class_for_url(url)
                 else:
-                    if hasattr(handler_module, 'serial_class_for_url'):
-                        url, klass = handler_module.serial_class_for_url(url)
-                    else:
-                        klass = handler_module.Serial
-                    break
+                    klass = handler_module.Serial
             else:
                 raise ValueError('invalid URL, protocol {!r} not known'.format(protocol))
     # instantiate and open when desired
