1ea8e2496685d7f41ec4a003606a3034ed018e79
Kurt McKee <contactme@kurtmckee.org>
2025-12-06 12:11:49 -0600

    Update/modernize the code in `list_ports_linux.py`
    This introduces the following changes:
    
    * Type annotations have been added.
    * The code has been run through mypy.
      Some issues were found and fixed,
      but others will require additional work.
    * The code has been run through pyupgrade and isort.
    * Device patterns (like `/dev/ttyS*`) have been moved,
      and have been reordered to reflect which patterns have tests
      and which are currently untested.
    * The `if __name__ == "__main__"` condition has been removed.
      If seeing the device subsystem is valuable,
      the `pyserial-ports` command can be updated for that purpose.
    * Some typos were fixed.

--- serial/tools/list_ports_linux.py
+++ serial/tools/list_ports_linux.py
@@ -8,28 +8,30 @@
 #
 # SPDX-License-Identifier:    BSD-3-Clause
 
-from __future__ import absolute_import
-
 import glob
 import os
+import typing
+
 from serial.tools import list_ports_common
 
 
 class SysFS(list_ports_common.ListPortInfo):
     """Wrapper for easy sysfs access and device info"""
 
-    def __init__(self, device):
-        super(SysFS, self).__init__(device)
+    def __init__(self, device: str) -> None:
+        super().__init__(device)
         # special handling for links
-        if device is not None and os.path.islink(device):
+        if os.path.islink(device):
             device = os.path.realpath(device)
             is_link = True
         else:
             is_link = False
         self.usb_device_path = None
-        if os.path.exists('/sys/class/tty/{}/device'.format(self.name)):
-            self.device_path = os.path.realpath('/sys/class/tty/{}/device'.format(self.name))
-            self.subsystem = os.path.basename(os.path.realpath(os.path.join(self.device_path, 'subsystem')))
+        if os.path.exists(f'/sys/class/tty/{self.name}/device'):
+            self.device_path = os.path.realpath(f'/sys/class/tty/{self.name}/device')
+            self.subsystem = os.path.basename(
+                os.path.realpath(os.path.join(self.device_path, 'subsystem')),
+            )
         else:
             self.device_path = None
             self.subsystem = None
@@ -61,12 +63,11 @@ class SysFS(list_ports_common.ListPortInfo):
             self.product = self.read_line(self.usb_device_path, 'product')
             self.interface = self.read_line(self.usb_interface_path, 'interface')
 
-        if self.subsystem in ('usb', 'usb-serial'):
+        # Transform .description and .hwid keys.
+        if self.subsystem in ('usb', 'usb-serial'):  # USB
             self.description = self.usb_description()
             self.hwid = self.usb_info()
-
-        #~ elif self.subsystem in ('pnp', 'amba'):  # PCI based devices, raspi
-        elif self.subsystem == 'pnp':  # PCI based devices
+        elif self.subsystem == 'pnp':  # PCI-based devices
             self.description = self.name
             self.hwid = self.read_line(self.device_path, 'id')
         elif self.subsystem == 'amba':  # raspi
@@ -74,38 +75,40 @@ class SysFS(list_ports_common.ListPortInfo):
             self.hwid = os.path.basename(self.device_path)
 
         if is_link:
-            self.hwid += ' LINK={}'.format(device)
+            self.hwid += f' LINK={device}'
 
-    def read_line(self, *args):
+    @staticmethod
+    def read_line(arg: str, *args: str) -> str | None:
         """\
         Helper function to read a single line from a file.
         One or more parameters are allowed, they are joined with os.path.join.
-        Returns None on errors..
+        Returns None on errors.
         """
         try:
-            with open(os.path.join(*args)) as f:
+            with open(os.path.join(arg, *args)) as f:
                 line = f.readline().strip()
             return line
-        except IOError:
+        except OSError:
             return None
 
 
-def comports(include_links=False, hide_subsystems=[]):
+def comports(
+    include_links: bool = False, hide_subsystems: typing.Container[str] | None = None
+) -> list[SysFS]:
+    if hide_subsystems is None:
+        hide_subsystems = ()
+
     devices = set()
     with open('/proc/tty/drivers') as drivers:
         for driver in drivers.readlines():
             items = driver.strip().split()
             if items[4] == 'serial':
-                devices.update(glob.glob(items[1]+'*'))
+                devices.update(glob.glob(items[1] + '*'))
 
     if include_links:
         devices.update(list_ports_common.list_links(devices))
-    return [info
-            for info in [SysFS(d) for d in devices]
-            if info.subsystem not in hide_subsystems]
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# test
-if __name__ == '__main__':
-    for info in sorted(comports()):
-        print("{0}: {0.subsystem}".format(info))
+    return [
+        info
+        for info in [SysFS(d) for d in devices]
+        if info.subsystem not in hide_subsystems
+    ]
